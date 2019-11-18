d1c56d1347a7d25c4a4f55af46be56a4026c9980
Samuel Weiss <samuel.weiss@compass.com>
2019-11-18 16:04:41 -0500

    push

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, send_from_directory
 
 from s3_downloader import S3Downloader
 
@@ -12,3 +12,10 @@ def index():
     return render_template('landing.html', file=file)
 
 
+@app.route('/statics/assets/<path:path>')
+def send_js(path):
+    return send_from_directory('statics/assets', path)
+
+@app.route('/statics/data/<path:path>')
+def send_data(path):
+    return send_from_directory('statics/data', path)
