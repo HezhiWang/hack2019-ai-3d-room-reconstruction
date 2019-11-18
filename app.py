9c41e6b82e57c9165a201ab24863855eb635e719
Oliver Sctt <oliver.scott@compass.com>
2019-11-18 15:54:37 -0500

    standing up application on heroku

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def index():
+    target_name = request.args.get('listing_id', '')
+
     # Check if there is a new ODM file on s3 and if so, download it and send it to the frontend
     s3_client = S3Downloader()
-    file = s3_client.poll_bucket()
-    return render_template('landing.html', file=file)
-
-
+    zip = s3_client.poll_bucket(target_name=target_name)
+    return render_template('landing.html', file=zip)
--- s3_downloader.py
+++ s3_downloader.py
@@ -12,18 +12,19 @@ class S3Downloader:
     def __init__(self):
         self.s3_client = boto3.client('s3')
 
-    def poll_bucket(self):
+    def poll_bucket(self, target_name):
         """
         Check if s3 bucket has an ODM file. If so, download it and return it.
         Note: if there are multiple ODM files, this will only return the first one found.
         :return:
         """
-        objects = self.s3_client.list_objects_v2(Bucket=BUCKET_NAME)['Contents']
-        for object in objects:
-            file_name = object['Key']
-            if ".obj" in file_name:
-                return self.download(file_name)
-        return None
+        objects = self.s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix='output/')['Contents']
+        targets = [object['Key'] for object in objects if object['Key'].endswith(target_name + '.zip')]
+        if not targets:
+            return None
+
+        return self.download(targets[0])
+
 
 
     def download(self, file_name):
