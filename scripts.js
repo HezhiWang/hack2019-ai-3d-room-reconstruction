2e483ebb051441891227bf45324df5ecc3c2e50e
HezhiWang <hw1567@nyu.edu>
2019-11-18 15:07:49 -0500

    js code to loading obj file

---
+++ scripts.js
@@ -0,0 +1,53 @@
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 40;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize();
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setTexturePath('/statics/data/odm_texturing/');
+mtlLoader.setPath('/statics/data/odm_texturing/');
+mtlLoader.load('odm_textured_model.mtl', function (materials) {
+
+    materials.preload();
+
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('/statics/data/odm_texturing/');
+    objLoader.load('odm_textured_model.obj', function (object) {
+
+        scene.add(object);
+        object.position.y -= 60;
+
+    });
+
+});
+
+var animate = function () {
+	requestAnimationFrame( animate );
+	controls.update();
+	renderer.render(scene, camera);
+};
+
+animate();
