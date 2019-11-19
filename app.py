2680a337f216a565bc4c3900b6b7b3c1cd5e751e
Samuel Weiss <samuel.weiss@compass.com>
2019-11-19 11:50:54 -0500

    Revert "web viewer"
    This reverts commit e6557d7e64a0cea79fc88caafd7d4b2111cc6067.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template, send_from_directory
+from flask import Flask, escape, request, render_template
 
 from s3_downloader import S3Downloader
 
@@ -11,22 +11,4 @@ def index():
     file = s3_client.poll_bucket()
     return render_template('landing.html', file=file)
 
-@app.route('/statics/<path:path>')
-def send_js(path):
-    return send_from_directory('statics', path)
 
-@app.route('/include/<path:path>')
-def send_include(path):
-    return send_from_directory('statics/include', path)
-
-@app.route('/jsmodeler/<path:path>')
-def send_jsmodeler(path):
-    return send_from_directory('statics/jsmodeler', path)
-
-@app.route('/extensions/<path:path>')
-def send_extensions(path):
-    return send_from_directory('statics/extensions', path)
-
-@app.route('/images/<path:path>')
-def send_images(path):
-    return send_from_directory('statics/images', path)
